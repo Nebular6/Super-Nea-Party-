0b7a4dc942c693e688a584a7064e77b151a921cf
jamie <jamieray.jamie@gmail.com>
2025-02-07 15:48:22 +0000

    asteroids good

--- Python Code.py
+++ Python Code.py
@@ -1,6 +1,5 @@
 import pygame
 import time
-import sched
 import math
 import random
 pygame.init()
@@ -9,14 +8,23 @@ clock = pygame.time.Clock()
 main_loop = True
 current_frame = 0
 
+
+
+
+
+
 def quitting_script():
+        """ subroutine to be called every game frame to allow users to quit whenever """
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                 pygame.display.quit()
                 return False
         return True
 
+
 def set_setbackground_image(rel_path):
+    """ i set windows with a background image, since i want to create multiple games i can utilise this subroutine to 
+        set one easily just using the relitive path to the image """
     pygame.display.quit()
     screen = pygame.display.set_mode()
     pygame.display.set_caption("Super Nea Party")
@@ -27,13 +35,17 @@ def set_setbackground_image(rel_path):
     util = [screen, backing_image]
     return util
 
+
 def check_if_win(screen_slots,new_screen_height,new_screen_width,symbols):
+    """ Simple Subroutine to return if the game state is currently "WON" or not """
     if symbols[0] == symbols[1] == symbols[2]:
         return False
     else:
         return True
 
+
 def draw_symbols(symbol_list,symbols,screen_slots,x_pos_slot_symbols):
+    """ Subroutine to create the different symbols whenever a new "spin" is played """
     symboll_list_usable = []
     for i in range(3):
         used_symbol = symbol_list[random.randint(0,2)]
@@ -46,6 +58,7 @@ def draw_symbols(symbol_list,symbols,screen_slots,x_pos_slot_symbols):
 
 
 def slots():
+    """ Main game loop for slots """
     global current_frame
     current_frame += 1
     pygame.display.quit()
@@ -84,19 +97,23 @@ def slots():
 
 
 class Sprite():
+    """ Main Sprite superclass to build other classes off of, has a screen image and position values """
+    
     def __init__(self, imagefilepath, position,screen):
         self.image = pygame.image.load(imagefilepath)
         self.position = position
         self.screen = screen
 
     def updateimage(self):
+        """ I gave the sprite class this subroutine so each subsequent class can update their position easily """
         self.screen.blit(self.image,self.position)
-
-    def checkcollision(self,ass):
-        pass
     
 
 class Ship(Sprite):
+    """ The ship class, subclass of sprite allows the additional attirubtes of 
+        its imagefilepath mainly to display the ship furthermore it has an angle 
+        of rotation  """
+    
     def __init__(self, imagefilepath, screen):
         position = 0
         super().__init__(imagefilepath, position ,screen)
@@ -107,12 +124,15 @@ class Ship(Sprite):
         self.timeofshoot = time.time()
 
     def position(self):
-
+        """ yep """
         return self.position
+    
     def current_angle(self):
+        """ yep.2 """
         return self.current_angle
 
     def ship_direction(self,keys):
+        """ Subroutine to allow the ship to rotate and updates its image as such using a and d  """
         req = self.time_of_movement - time.time()
         if req > -0.01:
             return
@@ -129,14 +149,17 @@ class Ship(Sprite):
             self.time_of_movement = time.time()
 
     def Shoot(self,screen):
+        """ adds a Projectile to the game in bullet list when shot to be updated ect """
         pygame.mixer.music.load("Assets\Sounds\laser-shot-ingame-230500.mp3")
         pygame.mixer.music.play()
         return Projectile("Assets/Asteroid/bullet.png",(0,0),screen,self)
 
 
 class Projectile(Sprite):
+    """ Class for bullets shot by script """
     def __init__(self, imagefilepath, position, screen, ship):
         super().__init__(imagefilepath, position, screen)
+        """ code to translate the current rotation of the ship into the direction of travel the bullet needs to go """
         self.position = (screen.get_width()/2-self.image.get_width()+50/2,screen.get_height()/2-self.image.get_width()+50)
         self.ship = ship
         self.angleoftravel = self.ship.current_angle + 90
@@ -144,13 +167,14 @@ class Projectile(Sprite):
         self.ymove = 10 * math.sin((-self.angleoftravel * (0.01745329)))  
 
     def updatepos(self):
+        """ moves the bullet in the direction of travel & updates its image """
         self.position = self.position[0]+self.xmove,self.position[1]+self.ymove
         self.updateimage()
 
 
 class Asteroid(Sprite):
     def __init__(self, imagefilepath,screen):
-        
+        """ initiates the asterioid in one of the random 4 corners  """
         # easy way to get an asteroid at atleast 1 maximum and randomised position so anywhere on outside of screen
         if random.randint(1,2) == 2:
             if random.randint(1,2) == 2:
@@ -166,6 +190,7 @@ class Asteroid(Sprite):
         self.speed = random.randint(1,5)  #random.randint(250,500)
     
     def towardsCenter(self,screen,asteroids):
+        """ Moves all asteroids towards the middle of the screen; Ship  """
         try:
             self.x_difference = screen.get_width() / 2 - self.position[0]
             self.y_difference = screen.get_height() / 2 - self.position[1]
@@ -181,6 +206,7 @@ class Asteroid(Sprite):
             
 
     def checkcollision(self, bullet,asteroids):
+        """ checks collision between all currently living asteroids for bullets """
         if self.position[0] < bullet.position[0] < self.position[0] + self.image.get_width():
             if self.position[1] < bullet.position[1]<self.position[1] + self.image.get_height():
                 asteroids.remove(self)
